92a0799778123bd26cfa0b0d3b231f8749bf1790
Adrien Dion <adion81@gmail.com>
2019-11-01 12:45:47 -0500

    deleted models and finish controller routes

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -1,10 +1,25 @@
 using Microsoft.AspNetCore.Mvc;
-using DojoSurvey.Models;
+using System;
 
 namespace DojoSurvey.Controllers
 {
     public class HomeController : Controller
     {
-        
+        [HttpGet("")]
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpPost("process")]
+        public IActionResult Survey(string name, string location, string language, string comments)
+        {
+            ViewBag.Name = name;
+            ViewBag.Location = location;
+            ViewBag.Language = language;
+            ViewBag.Comments = comments;
+            ViewBag.Date = DateTime.Now;
+            return View();
+        }
     }
 }
